823b9de8b6dd80baca0713206062546d2a1764fc
BenoBenq <boot.12345@gmx.de>
2015-10-24 23:18:08 +0200

    Changed Main.java

--- src/benobenq/fantasy_game_GUI/main/Main.java
+++ src/benobenq/fantasy_game_GUI/main/Main.java
@@ -1,10 +1,14 @@
 package benobenq.fantasy_game_GUI.main;
 
+import benobenq.fantasy_game_GUI.gui.GUIForm;
+
 /**
  * Created by Bent on 23.10.2015.
  */
 public class Main {
     public static void main(String[] args) {
-
+        GUIForm guif = new GUIForm();
     }
+
+
 }
