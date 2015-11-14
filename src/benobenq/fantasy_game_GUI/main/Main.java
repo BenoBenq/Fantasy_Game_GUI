005ed558c47e5d9ca7822e5ff8d89bdc64fad9ab
BenoBenq <boot.12345@gmx.de>
2015-11-14 21:24:53 +0100

    Changed Main.java

--- src/benobenq/fantasy_game_GUI/main/Main.java
+++ src/benobenq/fantasy_game_GUI/main/Main.java
@@ -1,13 +1,15 @@
 package benobenq.fantasy_game_GUI.main;
 
 import benobenq.fantasy_game_GUI.gui.GUIForm;
+import benobenq.fantasy_game_GUI.spielfeld.Kampfleitung;
 
 /**
  * Created by Bent on 23.10.2015.
  */
 public class Main {
     public static void main(String[] args) {
-        GUIForm guif = new GUIForm();
+        Kampfleitung kL = new Kampfleitung();
+        GUIForm guif = new GUIForm(kL);
     }
 
 
